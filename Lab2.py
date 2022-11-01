f3b033ed58562580838d97e047125e5112fd740e
EverleyTeo <everley.21@ichat.sp.edu.sg>
2022-11-01 21:15:11 +0800

    ET0735 Lab 2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,27 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    y = display_main_menu()
+    num_list = get_user_input(y)
+    calc_average(num_list)
+    calc_min_max_temperature(num_list)
+
+def display_main_menu():
+    x = input("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
+    return x
+
+def get_user_input(y):
+    values = y.split(",")
+    floats = [float(item) for item in values]
+    return floats
+
+def calc_average(z):
+    average = sum(z) / len(z)
+    average = round(average, 2)
+    print(average)
+
+def calc_min_max_temperature(z):
+    print(max(z))
+    print(min(z))
+
+if __name__ == "__main__":
+    main()
