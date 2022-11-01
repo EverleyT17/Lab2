481daad0fb71eaf7b6a3f0b3e2a531c2c576cdbe
EverleyTeo <everley.21@ichat.sp.edu.sg>
2022-11-01 21:37:33 +0800

    ET0735 Lab2

--- Lab2.py
+++ Lab2.py
@@ -1,9 +1,15 @@
+import statistics
+
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     y = display_main_menu()
     num_list = get_user_input(y)
     calc_average(num_list)
     calc_min_max_temperature(num_list)
+    temp_sort(num_list)
+    tempsort = temp_sort(num_list)
+    calc_median_temp(tempsort)
+
 
 def display_main_menu():
     x = input("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
@@ -20,8 +26,21 @@ def calc_average(z):
     print(average)
 
 def calc_min_max_temperature(z):
+    print("The max is: ")
     print(max(z))
+    print("The min is: ")
     print(min(z))
+
+def temp_sort(z):
+    tempsort = sorted(z)
+    print("Sorting numbers in ascending order: ")
+    print(tempsort)
+    return tempsort
+
+def calc_median_temp(tempsort):
+    z = statistics.median(tempsort)
+    print("The median temperature is: ")
+    print(z)
 
 if __name__ == "__main__":
     main()
